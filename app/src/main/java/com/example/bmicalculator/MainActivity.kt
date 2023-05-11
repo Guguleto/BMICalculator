07c239612c65190a08a5f01b1522aee6f949459b
gzitha021 <gzitha021@student.wethinkcode.co.za>
2023-05-11 16:03:06 +0200

    Initial commit

---
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.bmicalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val weightText = findViewById<EditText>(R.id.etWeight)
+        val heightText = findViewById<EditText>(R.id.etHeight)
+        val calcButton = findViewById<Button>(R.id.btnCalculator)
+
+        calcButton.setOnClickListener {
+            val weight = weightText.text.toString()
+            val height = heightText.text.toString()
+
+            val bmi = weight.toFloat()/((height.toFloat()/100)*(height.toFloat()/100))
+            val bmi2Digits = String.format("%.2f",bmi).toFloat()
+            displayResult(bmi2Digits)
+
+        }
+    }
+
+    private fun displayResult(bmi:Float){
+        val resultIndex = findViewById<TextView>(R.id.tvindex)
+        val resultDescription = findViewById<TextView>(R.id.tvResult)
+        val info = findViewById<TextView>(R.id.tvInfo)
+
+        resultIndex.text = bmi.toString()
+        info.text = "(normal range is 18.5 - 24.9)"
+
+        var resultText = ""
+        var color = 0
+
+    }
+}
